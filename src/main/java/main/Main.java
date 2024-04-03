6adb5bee1817982fdc998912a280a8def82e4979
ToorBeezy <olishuk-1@mail.ru>
2024-04-03 19:12:24 +0500

    2.3 Advanced language basics

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,31 @@
 package main;
 
+import accounts.AccountService;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
+import servlets.SignUpServlet;
+import servlets.SignInServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        AccountService accountService = new AccountService();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] {resource_handler, context});
 
         Server server = new Server(8080);
-        server.setHandler(context);
+        server.setHandler(handlers);
 
         server.start();
         System.out.println("Server started");
