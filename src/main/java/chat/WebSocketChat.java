433509a2205037eda31a0c9a24ed81909f91a80b
ToorBeezy <olishuk-1@mail.ru>
2024-05-14 02:43:45 +0500

    4.2 Tools fixed

---
+++ src/main/java/chat/WebSocketChat.java
@@ -0,0 +1,20 @@
+package chat;
+
+import java.io.IOException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.*;
+
+@WebSocket
+public class WebSocketChat {
+    private Session sess;
+
+    @OnWebSocketConnect
+    public void onConnect(Session sess) {
+        this.sess = sess;
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String message) throws IOException {
+        sess.getRemote().sendString(message);
+    }
+}
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,61 +1,30 @@
 package main;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import userServer.UserServer;
-import userServer.UserServerController;
-import userServer.UserServerControllerMBean;
-import userServer.UserServerImplement;
-import servlets.AdminPageServlet;
-import servlets.HomePageServlet;
-
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
-import java.lang.management.ManagementFactory;
-
+import chat.WebSocketChatServlet;
 
 public class Main {
-
-    static final Logger logger = LogManager.getLogger(Main.class.getName());
-
     public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
 
-        String portString = "8080";
-        int port = Integer.valueOf(portString);
-
-        logger.info("Starting at http://127.0.0.1:" + portString);
-
-        UserServerImplement userServer = new UserServer();
-
-        UserServerControllerMBean serverStatistics = new UserServerController(userServer);
-        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-
-        ObjectName name = new ObjectName("Admin:type=AccountServerController.usersLimit");
-        mbs.registerMBean(serverStatistics, name);
-
-        Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase("public_html");
         
-        context.addServlet(new ServletHolder(new HomePageServlet(userServer)), HomePageServlet.PAGE_URL);
-        context.addServlet(new ServletHolder(new AdminPageServlet(userServer)), AdminPageServlet.PAGE_URL);
-
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setDirectoriesListed(true);
-        resource_handler.setResourceBase("static");
-
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{resource_handler, context});
+        handlers.setHandlers(new Handler[] {resourceHandler, context});
+
         server.setHandler(handlers);
 
         server.start();
-        logger.info("Server started");
-        System.out.println("Server started");
+        System.out.println("Server started!");
         server.join();
     }
 }
